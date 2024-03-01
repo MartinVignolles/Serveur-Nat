55da809dcc28cd5e45577318ee7d7e4463acdaf0
Etienne Lopez <lopezetienne@wanadoo.fr>
2024-03-01 09:58:44 +0100

    etienne

---
+++ docker-raspberry-nginx/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Faites un appel API pour obtenir le port (remplacez l'URL par votre API)
+PORT=$(curl -s http://exemple-api.com/api/port)
+
+# Établissez une connexion SSH inversée en utilisant le port obtenu
+ssh -R "${PORT}":localhost:80 user@172.17.0.5
+
+# Gardez Nginx en cours d'exécution pour servir le contenu
+nginx -g 'daemon off;'
