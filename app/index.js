32fd50d344b724514ab9a3a6fd9cd312ed0fd2fd
kevin <kevinr12.mt@gmail.com>
2024-03-01 14:21:39 +0100

    airnet

--- app/index.js
+++ app/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const app = express();app.get('/', (req, res) => {
-  res.send('Hello World!')
+const path = require('path');
+const app = express();
+
+// Définir le répertoire des fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
-app.listen(5000, () => console.log('Server is up and running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
