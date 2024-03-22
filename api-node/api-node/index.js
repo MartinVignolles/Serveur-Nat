73ce93272b0455f8580894af5da327bf73297c56
Matias1512 <matias.caturla@gmail.com>
2024-03-22 10:04:03 +0100

    ça marche les ports

--- api-node/api-node/index.js
+++ api-node/api-node/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -12,10 +13,9 @@ const connection = mysql.createConnection({
     database: 'ports'
 });
 
-app.post('/addData', (req, res) => { 
+app.post('/getPort', async (req, res) => {
   const mac_adresse = req.body.mac_adresse;
-  const port = req.body.port;
-  
+
   const queryUse = connection.query('USE db_airlux', (err, result) => {
     if (err) {
         console.error('Erreur lors de l\'ajout de données:', err);
@@ -24,19 +24,45 @@ app.post('/addData', (req, res) => {
     console.log('Connecté à db_airlux');
   });
 
-  const queryAdd = connection.query('INSERT INTO ports (mac_adresse, port) VALUES (\''+mac_adresse+'\', '+port+');', (err, result) => {
+  try {
+    // Vérifier si l'adresse MAC a déjà un port attribué
+    const querySelect = connection.query('SELECT port FROM ports WHERE mac_adresse = ?', [mac_adresse], (err, result) => {
       if (err) {
-          console.error('Erreur lors de l\'ajout de données:', err);
-          return res.status(500).send('Erreur lors de l\'ajout de données');
+        console.error('Erreur lors de la recherche en base de données:', err);
+        return res.status(500).send('Erreur lors de la recherche en base de données');
+      } else {
+        if (result.length > 0) { // Vérifier si un port a été trouvé
+          return res.status(200).send(result[0]);
+        } else {
+          // Si pas de port attribué, demander au serveur
+          const apiUrl = 'http://nodeserver:5000/port';
+          axios.get(apiUrl)
+            .then(response => {
+              const newPort = response.data.port;
+              // Ajouter l'adresse MAC et le port dans la base de données
+              const queryAdd = connection.query('INSERT INTO ports (mac_adresse, port) VALUES (?, ?)', [mac_adresse, newPort], (err, result) => {
+                if (err) {
+                  console.error('Erreur lors de l\'ajout de données:', err);
+                  return res.status(500).send('Erreur lors de l\'ajout de données');
+                }
+                console.log('Données ajoutées avec succès !');
+              });
+              // Renvoyer le nouveau port
+              res.status(200).send({"port" : newPort});
+            })
+            .catch(error => {
+              console.error('Erreur lors de l\'appel à l\'API externe :', error);
+              res.status(500).json({ error: 'Erreur lors de l\'appel à l\'API externe' });
+            });
+        }
       }
-      console.log('Données ajoutées avec succès !');
-      res.status(200).send('Données ajoutées avec succès !');
-  });
+    });
+  } catch (error) {
+    console.error('Erreur lors de l\'exécution de la requête SQL :', error);
+    res.status(500).send('Erreur lors de l\'exécution de la requête SQL');
+  }
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- app/index.js
+++ app/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const portfinder = require('portfinder');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+
+app.get('/port', async (req, res) => {
+  try {
+    const port = await portfinder.getPortPromise();
+    if (typeof port === 'number' && Number.isInteger(port) && port > 0 && port <= 65535) {
+      // Vérifie si le port est un nombre entier positif dans la plage des ports valides
+      res.status(200).json({"port" : port.toString()});
+    } else {
+      res.status(500).send('Port invalide obtenu');
+    }
+  } catch (err) {
+    console.error('Erreur lors de la recherche de port :', err);
+    res.status(500).send('Erreur lors de la recherche de port');
+  }
+});
+
 app.listen(5000, () => console.log('Server is up and running'));
